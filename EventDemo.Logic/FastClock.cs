9d51e52fd38b5ae0f95639e1c99ff5cb78238d77
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2020-01-09 18:17:59 +0100

    Add project files.

---
+++ EventDemo.Logic/FastClock.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace EventDemo.Logic
+{
+    public class FastClock
+    {
+        private readonly DispatcherTimer _timer;
+        private bool _isRunning;
+
+        public event EventHandler<DateTime> OneMinuteIsOver;
+
+        public DateTime CurrentTime { get; private set; }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            //get { return _isRunning; }
+
+            set
+            {
+                if (!_isRunning && value)
+                {
+                    _timer.Start();
+                }
+                else if(_isRunning && value == false)
+                {
+                    _timer.Stop();
+                }
+
+                _isRunning = value;
+            }
+        }
+
+        public FastClock(DateTime currentTime)
+        {
+            CurrentTime = currentTime;
+            _timer = new DispatcherTimer();
+            _timer.Tick += OnTimerTick;
+            _timer.Interval = TimeSpan.FromMilliseconds(1000);
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            CurrentTime = CurrentTime.AddMinutes(1);
+            OnOneMinuteIsOver(CurrentTime);
+        }
+
+        protected virtual void OnOneMinuteIsOver(DateTime time)
+        {
+            OneMinuteIsOver?.Invoke(this, time);
+        }
+
+    }
+}
---
+++ EventDemo.UI/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EventDemo.Logic;
+
+namespace EventDemo.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FastClock _fastClock;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnInitialized(object sender, EventArgs e)
+        {
+            _fastClock = new FastClock(DateTime.Now);
+            _fastClock.OneMinuteIsOver += FastClockOneMinuteIsOver;
+            
+        }
+
+        private void FastClockOneMinuteIsOver(object sender, DateTime e)
+        {
+            txtTime.Text = $"Aktuelle Zeit: {e.ToShortTimeString()}";
+        }
+
+        private void ChkIsRunning_Click(object sender, RoutedEventArgs e)
+        {
+            _fastClock.IsRunning = chkIsRunning.IsChecked == true;
+        }
+    }
+}
